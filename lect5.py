c4b0a86f1e627801ff1d20d74e31f3e99f85760e
Barış Karakulak <32600733+RenkliKup@users.noreply.github.com>
2021-03-26 21:40:36 +0300

    Update lect5.py

--- lect5.py
+++ lect5.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder,PolynomialFeatures
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 class reg_:
